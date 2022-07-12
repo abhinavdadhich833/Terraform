823a4581cdb75b22b4a12eb7f25cdaeff4e0d000
kohlishubh <74098169+kohlishubh@users.noreply.github.com>
2022-07-12 09:50:03 +0530

    Update main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -52,3 +52,16 @@ from_port = 22
    cidr_blocks = ["0.0.0.0/0"]
  }
 }
+resource "aws_db_instance" "default" {
+allocated_storage = 20
+identifier = "testrds"
+storage_type = "gp2"
+engine = "mysql"
+engine_version = "5.7"
+instance_class = "db.t2.micro"
+name = "mydb"
+username = "admin"
+password = "admin123"
+parameter_group_name = "default.mysql5.7"
+skip_final_snapshot = true
+}
